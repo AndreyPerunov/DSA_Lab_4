718d64f5bacd25a691c8f33eaf48c3cb4c0a1c8a
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-11-26 18:42:01 +0200

    Implemented tree, addNode and treeHeight

--- main.cpp
+++ main.cpp
@@ -3,12 +3,95 @@
 #include <string>
 #include <vector>
 
+// Requirements: build binary sorted tree
+class Tree {
+  struct Node {
+    int data;
+    int height;
+    Node* left;
+    Node* right;
+  };
+public:
+  Node* root;
+
+  Tree() {
+    root = nullptr;
+  }
+
+  Tree(Node* node) {
+    root = node;
+  }
+
+  Node* createNode(int data) {
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->height = 0;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    return newNode;
+  }
+
+  // Task for all: prevent to add non unique number give error✅
+  void addElement(Node* currentNode, Node* newNode) {
+    if (root == nullptr) {
+      root = newNode;
+      return;
+    }
+    if (newNode->data < currentNode->data) {
+      if (currentNode->left == nullptr) {
+        currentNode->left = newNode;
+      } else {
+        addElement(currentNode->left, newNode);
+      }
+    } else if (newNode->data > currentNode->data) {
+      if (currentNode->right == nullptr) {
+        currentNode->right = newNode;
+      } else {
+        addElement(currentNode->right, newNode);
+      }
+    } else {
+      std::cout << "Error: non unique number" << std::endl;
+    }
+
+    updateHeight(currentNode);
+    // balanceTree(currentNode);
+  }
+
+  // ✅
+  int treeHeight(Node* root) {
+    return root == nullptr ? -1 : root->height;
+  }
+
+  // ✅
+  void updateHeight(Node* node) {
+    node->height = std::max(treeHeight(node->left), treeHeight(node->right)) + 1;
+  }
+  
+  void deleteElement(Node* node);
+  // Task 1. Traverse: Level order traversal: Visits nodes level-by-level and in left-to-right fashion at the same level.
+  void printTree(Node* root);
+  // Requirements: Implement a tree element search by a given value.
+  Node* getNode(int data);
+  // Task 2. Balance: Double Rotation
+  void balanceTree(Node* root);
+};
 
 int main() {
   // Declare Tree  
+  Tree tree;
+
+  tree.addElement(tree.root, tree.createNode(20));
+  tree.addElement(tree.root, tree.createNode(10));
+  tree.addElement(tree.root, tree.createNode(40));
+  tree.addElement(tree.root, tree.createNode(30));
+  tree.addElement(tree.root, tree.createNode(50));
+  tree.addElement(tree.root, tree.createNode(21));
+
+  tree.printTree(tree.root);
+  return 0;
 
   // Read Data
-  std::ifstream file("");
+  std::ifstream file("delta.txt");
 
   if (!file.is_open() || !file) {
     std::cerr << "Error opening file" << std::endl;
@@ -19,7 +102,8 @@ int main() {
 
   while (getline(file, line)) {
     try {      
-
+      int number = std::stoi(line);
+      tree.addElement(tree.root, tree.createNode(number));
     } catch (const std::invalid_argument& e) {
       std::cerr << "Error reading line number." << std::endl;
     }
@@ -27,12 +111,6 @@ int main() {
 
   file.close();
   std::cout << std::endl;
-
-  // Build Binary Search Tree from file (There should be only tree no additional structures)
-  // prevent to add non unique number give error
-  // void addElement(node)
-  // void deleteElement(node)
-  // int treeHeight(root)
 
   // Task 1. Traverse: Level order traversal: Visits nodes level-by-level and in left-to-right fashion at the same level.  
 
